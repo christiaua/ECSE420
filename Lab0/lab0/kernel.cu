f75f4d483d7f1298dde15c0d2e680dbb0ab28286
Yiming Wu <yiming.wu@mail.mcgill.ca>
2020-09-26 15:57:31 -0400

    File restructuring

---
+++ Lab0/lab0/kernel.cu
@@ -0,0 +1,17 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "lodepng.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+__global__ void cuda_hello() {
+    printf("Hello World from GPU!\n");
+}
+
+int main() {
+    cuda_hello << <1, 1 >> > ();
+    return 0;
+}
